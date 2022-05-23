b6275fd573f3b9e0a48fee2a7b68d29718a11bcc
Artem Pustovoit <ampustovoit@gmail.com>
2022-05-23 16:17:51 +0200

    first commit

---
+++ src/core/config.js
@@ -0,0 +1,16 @@
+// Replace with your own firebase config!
+export const FIREBASE_CONFIG = {
+  apiKey: 'AIzaSyDpZ4tKvKQY2L5gwa1DcosuSg2GqPp8Yes',
+  authDomain: 'finesseapp-33ceb.firebaseapp.com',
+  projectId: 'finesseapp-33ceb',
+  storageBucket: 'finesseapp-33ceb.appspot.com',
+  messagingSenderId: '396208222060',
+  appId: '1:396208222060:web:152ec77ed6f01133584d49',
+}
+
+// Replace with your own IDs! follow the guides here:
+// https://docs.expo.io/versions/latest/sdk/google/#using-it-inside-of-the-expo-app
+export const ANDROID_GOOGLE_CLIENT_ID =
+  '396208222060-bvp8q6pmh0vll9ebl5vgev4lleegkb66.apps.googleusercontent.com'
+export const IOS_GOOGLE_CLIENT_ID =
+  '396208222060-8cbcbaj3av5ukpeihee667pesj942b8r.apps.googleusercontent.com'
